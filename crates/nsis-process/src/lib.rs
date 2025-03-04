c8e2192192767c5e09e00eba9e1f7629d996ce1a
zhaoyunjie <zhaoyunjie.sh@chinaccs.cn>
2025-03-04 14:28:22 +0800

    Use std::process::Command in run_as_user

--- crates/nsis-process/src/lib.rs
+++ crates/nsis-process/src/lib.rs
@@ -250,92 +250,13 @@ fn get_processes(name: &str) -> Vec<u32> {
 ///
 /// Ported from https://devblogs.microsoft.com/oldnewthing/20190425-00/?p=102443
 unsafe fn run_as_user(program: &str, arguments: &str) -> bool {
-    let hwnd = GetShellWindow();
-    if hwnd.is_null() {
-        return false;
-    }
-
-    let mut proccess_id = 0;
-    if GetWindowThreadProcessId(hwnd, &mut proccess_id) == FALSE as u32 {
-        return false;
-    }
-
-    let process = OwnedHandle::new(OpenProcess(PROCESS_CREATE_PROCESS, FALSE, proccess_id));
-    if process.is_invalid() {
-        return false;
-    }
-
-    let mut size = 0;
-    if !(InitializeProcThreadAttributeList(ptr::null_mut(), 1, 0, &mut size) == FALSE
-        && GetLastError() == ERROR_INSUFFICIENT_BUFFER)
-    {
-        return false;
-    }
-
-    let mut buffer = vec![0u8; size];
-    let attribute_list = buffer.as_mut_ptr() as LPPROC_THREAD_ATTRIBUTE_LIST;
-    if InitializeProcThreadAttributeList(attribute_list, 1, 0, &mut size) == FALSE {
-        return false;
-    }
-
-    if UpdateProcThreadAttribute(
-        attribute_list,
-        0,
-        PROC_THREAD_ATTRIBUTE_PARENT_PROCESS as _,
-        &*process as *const _ as _,
-        mem::size_of::<HANDLE>(),
-        ptr::null_mut(),
-        ptr::null(),
-    ) == FALSE
-    {
-        return false;
-    }
-
-    let startup_info = STARTUPINFOEXW {
-        StartupInfo: STARTUPINFOW {
-            cb: mem::size_of::<STARTUPINFOEXW>() as _,
-            ..mem::zeroed()
-        },
-        lpAttributeList: attribute_list,
-    };
-    let mut process_info: PROCESS_INFORMATION = mem::zeroed();
-    let mut command_line = "\"".to_owned() + program + "\"";
-    if !arguments.is_empty() {
-        command_line.push(' ');
-        command_line.push_str(arguments);
-    }
-
-    let program_wide = encode_utf16(program);
-
-    if CreateProcessW(
-        program_wide.as_ptr(),
-        encode_utf16(&command_line).as_mut_ptr(),
-        ptr::null(),
-        ptr::null(),
-        FALSE,
-        CREATE_UNICODE_ENVIRONMENT | CREATE_NEW_PROCESS_GROUP | EXTENDED_STARTUPINFO_PRESENT,
-        ptr::null(),
-        ptr::null(),
-        &startup_info as *const _ as _,
-        &mut process_info,
-    ) != FALSE
-    {
-        CloseHandle(process_info.hProcess);
-        CloseHandle(process_info.hThread);
-        true
-    } else if GetLastError() == ERROR_ELEVATION_REQUIRED {
-        let result = ShellExecuteW(
-            ptr::null_mut(),
-            w!("open"),
-            program_wide.as_ptr(),
-            encode_utf16(&command_line).as_ptr(),
-            ptr::null(),
-            SW_SHOW,
-        );
-        result as isize > 32
-    } else {
-        false
-    }
+    let parent_path = std::path::Path::new(program).parent().unwrap();
+    let _ = std::process::Command::new(&program)
+        .args(&arguments)
+        .current_dir(parent_path)
+        .spawn()
+        .expect("failed to execute process");
+    true
 }
 
 struct OwnedHandle(HANDLE);
